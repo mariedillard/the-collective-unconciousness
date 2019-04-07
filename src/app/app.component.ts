6c584730df08df0cf76f6bc3e1d8034faa7e6924
Marie Dillard <mariedillard@protonmail.com>
2019-04-07 15:25:21 -0700

    updates non-working function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  start time = new Date();
-  var startMonth = this.startTime.getMonth();
-  var startDay = this.startTime.getDay();
-  var startYear = this.startTime.getFullYear();
-  var startHour = this.startTime.getHours();
-  var startMinutes = this.startTime.getMinutes();
-  var startSeconds = this.startTime.getSeconds();
+  startTime = new Date();
+  startMonth = this.startTime.getMonth();
+  startDay = this.startTime.getDay();
+  startYear = this.startTime.getFullYear();
+  startHour = this.startTime.getHours();
+  startMinute = this.startTime.getMinutes();
+  startSecond = this.startTime.getSeconds();
+  timeSpent;
   
-  timeHere = function(currentTime, currentMonth, currentDate, currentDay, currentYear, currentHour, currentMinute, currentSecond, timeSpent) {
+  timeHere = function(currentTime, currentMonth, currentDate, currentDay, currentYear, currentHour, currentMinute, currentSecond, monthsSpent, daysSpent, yearsSpent, hoursSpent, minutesSpent, secondsSpent) {
     currentTime = new Date();
     currentMonth = this.currentTime.getMonth();
     currentDay = this.currentTime.getDay();
@@ -23,6 +24,27 @@ export class AppComponent {
     currentMinute = this.currentTime.getMinutes();
     currentSecond = this.currentTime.getSeconds();
     
-    timeSpent = startMonth - ;
-  };
+    monthsSpent = this.currentMonth - this.startMonth;
+    daysSpent = this.currentDay - this.startDay;
+    yearsSpent = this.currentYear - this.startYear;
+    hoursSpent = this.currentHour - this.startHour;
+    minutesSpent = this.currentMinute - this.startMinute;
+    secondsSpent = this.currentSecond - this.startSecond;
+
+    if(minutesSpent <= 0) {
+      this.timeSpent = secondsSpent + " seconds.";
+    } else if(hoursSpent <= 0) {
+      this.timeSpent = minutesSpent + " minutes " + secondsSpent + " seconds.";
+    } else if(daysSpent <= 0) {
+      this.timeSpent = hoursSpent + " hours " + minutesSpent + " minutes " + secondsSpent + " seconds.";
+    } else if (monthsSpent <= 0) {
+      this.timeSpent = daysSpent + " days " +hoursSpent + " hours " + minutesSpent + " minutes " + secondsSpent + " seconds.";
+    } else {
+
+    }
+  }
+  $scope.useInterval = function() {
+    //Show current seconds value 5 times after every 1000 ms
+    $interval(updateExampleText, 1000, 5);
+
 }
