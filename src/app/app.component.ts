915e15fc22d1824974f001b935cd16c5c533c410
Marie Dillard <mariedillard@protonmail.com>
2019-04-07 14:48:08 -0700

    updates non-working function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  startTime = new Date();
-
-  currentMonth = this.startTime.getMonth();
-  currentDay = this.startTime.getDay();
-  currentYear = this.startTime.getFullYear();
-  currentHour = this.startTime.getHours();
-  currentMinutes = this.startTime.getMinutes();
-  currentSeconds = this.startTime.getSeconds();
-
-  function($scope, $interval) {
-    $scope.clock = new Date();
-    var updateClock = function() {
-      $scope.clock = new Date();
-    };
-    $interval(updateClock, 1000);
-  }
+  start time = new Date();
+  var startMonth = this.startTime.getMonth();
+  var startDay = this.startTime.getDay();
+  var startYear = this.startTime.getFullYear();
+  var startHour = this.startTime.getHours();
+  var startMinutes = this.startTime.getMinutes();
+  var startSeconds = this.startTime.getSeconds();
+  
+  timeHere = function(currentTime, currentMonth, currentDate, currentDay, currentYear, currentHour, currentMinute, currentSecond, timeSpent) {
+    currentTime = new Date();
+    currentMonth = this.currentTime.getMonth();
+    currentDay = this.currentTime.getDay();
+    currentYear = this.currentTime.getFullYear();
+    currentHour = this.currentTime.getHours();
+    currentMinute = this.currentTime.getMinutes();
+    currentSecond = this.currentTime.getSeconds();
+    
+    timeSpent = startMonth - ;
+  };
 }
