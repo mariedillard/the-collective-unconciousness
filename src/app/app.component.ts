831d373220f9a2002d127dbec939044dfcdbb16f
Marie Dillard <mariedillard@protonmail.com>
2019-04-07 18:30:26 -0700

    updates non-working function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,38 @@ import { initDomAdapter } from '@angular/platform-browser/src/browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+ 
+  startTime = new Date();
+  startMonth = this.startTime.getMonth();
+  startDay = this.startTime.getDay();
+  startYear = this.startTime.getFullYear();
+  startHour = this.startTime.getHours();
+  startMinute = this.startTime.getMinutes();
+  startSecond = this.startTime.getSeconds();
+  ngOnInit() {
+    var currentTime = new Date();
+    //var currentMonth = currentTime.getMonth();
+    var currentDay = currentTime.getDay();
+    //var currentYear = currentTime.getFullYear();
+    var currentHour = currentTime.getHours();
+    var currentMinute = currentTime.getMinutes();
+    var currentSecond = currentTime.getSeconds();
+    
+   // var monthsSpent = currentMonth - this.startMonth;
+    var daysSpent = currentDay - this.startDay;
+    //var yearsSpent = currentYear - this.startYear;
+    var hoursSpent = currentHour - this.startHour;
+    var minutesSpent = currentMinute - this.startMinute;
+    var secondsSpent = currentSecond - this.startSecond;
+
+    if(minutesSpent <= 0) {
+      console.log(secondsSpent + " seconds.");
+    } else if(hoursSpent <= 0) {
+      console.log(minutesSpent + " minutes " + secondsSpent + " seconds.");
+    } else if(daysSpent <= 0) {
+      console.log(hoursSpent + " hours " + minutesSpent + " minutes " + secondsSpent + " seconds.");
+    } else {
+      console.log("error");
+    }
+  }
 }
--- src/app/user-timer/user-timer.component.ts
+++ src/app/user-timer/user-timer.component.ts
@@ -6,38 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-timer.component.css']
 })
 export class UserTimerComponent implements OnInit {
-  startTime = new Date();
-  startMonth = this.startTime.getMonth();
-  startDay = this.startTime.getDay();
-  startYear = this.startTime.getFullYear();
-  startHour = this.startTime.getHours();
-  startMinute = this.startTime.getMinutes();
-  startSecond = this.startTime.getSeconds();
-  timeHere() {
-    var currentTime = new Date();
-    //var currentMonth = currentTime.getMonth();
-    var currentDay = currentTime.getDay();
-    //var currentYear = currentTime.getFullYear();
-    var currentHour = currentTime.getHours();
-    var currentMinute = currentTime.getMinutes();
-    var currentSecond = currentTime.getSeconds();
-    
-   // var monthsSpent = currentMonth - this.startMonth;
-    var daysSpent = currentDay - this.startDay;
-    //var yearsSpent = currentYear - this.startYear;
-    var hoursSpent = currentHour - this.startHour;
-    var minutesSpent = currentMinute - this.startMinute;
-    var secondsSpent = currentSecond - this.startSecond;
-
-    if(minutesSpent <= 0) {
-      return secondsSpent + " seconds.";
-    } else if(hoursSpent <= 0) {
-      return minutesSpent + " minutes " + secondsSpent + " seconds.";
-    } else if(daysSpent <= 0) {
-      return hoursSpent + " hours " + minutesSpent + " minutes " + secondsSpent + " seconds.";
-    } 
-  };
-
+  
   ngOnInit() {
   }
 
